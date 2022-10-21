07e39d71286f94f122d03da90428d935b03bf3fa
darman84 <zacharykeith2000@gmail.com>
2022-10-20 20:37:59 -0500

    Add compareTo

--- Printjob.java
+++ Printjob.java
@@ -1,14 +1,29 @@
+import javax.lang.model.util.ElementScanner6;
+
 public class Printjob implements Comparable<Printjob>
 {
 
-    public Printjob(String name, int priority, int numPages)
+    private String username;
+    private int userpriority;
+    private int numPages;
+
+    public Printjob(String username, int userpriority, int numPages)
     {
-        this.name = name;
-        this.priority = priority;
+        this.username = username;
+        this.userpriority = userpriority;
         this.numPages = numPages;
     }
 
-    private String name;
-    private int priority;
-    private int numPages;
+    @Override
+    public int compareTo(Printjob myJob)
+    {
+        if(this.userpriority > myJob.userpriority)
+            return 1;
+        else if(this.userpriority == myJob.userpriority)
+            return 0;
+        else 
+            return -1;
+    }
+
+
 }
